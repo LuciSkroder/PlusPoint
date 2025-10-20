771797370d1eda8d0d382e020a12437372500d1f
LuciSkroder <Luci.skroder@gmail.com>
2025-10-20 10:52:06 +0200

    css paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./css/index.css";
+import "./css/root.css";
+import "./css/homepage.css";
+import "./css/shop.css";
+import "./css/tasks.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
