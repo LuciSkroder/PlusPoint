41c10eed3116e610104cf5a5e95cebaba824427b
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-10-22 19:34:50 +0200

    nav bar update
    If/else statement added til navbar, så der er null ved login og logud ved home

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -4,7 +4,7 @@ import "../css/bottomnav.css";
 export default function BottomNav() {
   return (
     <nav className="bottom-nav">
-      <NavLink to="/">Placeholder</NavLink>
+      <NavLink to="/">Home</NavLink>
       <NavLink to="/add-child">Placeholder</NavLink>
       <NavLink to="/create">Placeholder</NavLink>
     </nav>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,38 @@
-import { NavLink } from "react-router";
+import { NavLink, useLocation, useNavigate } from "react-router";
 import PointCounter from "./PointCounter";
 import "../css/navbar.css";
 
-export default function NavBar() {
+function GåTilbage() {
+  const navigate = useNavigate();
   return (
-    <nav className="top-nav">
-      <NavLink to="/login">
-        <img src="../img/logout.png" alt="logout" className="logout-icon" />
-      </NavLink>
-      <PointCounter></PointCounter>
-      <NavLink to="/create">Create</NavLink>
-    </nav>
+    <button className="tilbage-knap" onClick={() => navigate(-1)}>
+      ➜
+    </button>
   );
+}
+
+export default function NavBar() {
+  const location = useLocation();
+
+  if (location.pathname === "/login") {
+    return null;
+  } else if (location.pathname === "/") {
+    return (
+      <nav className="top-nav">
+        <NavLink to="/login">
+          <img src="../img/logout.png" alt="logout" className="logout-icon" />
+        </NavLink>
+        <PointCounter></PointCounter>
+        <NavLink to="/create">Create</NavLink>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="top-nav">
+        <GåTilbage></GåTilbage>
+        <PointCounter></PointCounter>
+        <NavLink to="/create">Create</NavLink>
+      </nav>
+    );
+  }
 }
