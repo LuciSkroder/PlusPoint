e6da11423a40abd22a81a89542ff40952d1a459f
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-10-22 15:29:31 +0200

    opdateret pointtæller
    Pointtæller spiller nu med parent verification
    
    mangler at gemme pointene for forældre navbar

--- src/components/PointCounter.jsx
+++ src/components/PointCounter.jsx
@@ -5,17 +5,22 @@ import { ref, get, onValue, off } from "firebase/database";
 export default function PointCounter() {
     const [point, setPoint] = useState(0);
     const [bruger, setBruger] = useState(null);
-useEffect(() => {
-    const checkLogin = Auth.onAuthStateChanged((user) => {
-        setBruger(user);
-        if (user) {
-            const pointUpdate = ref(DataBase, `childrenProfiles/${user.uid}/points`);
-});
+                const pointHent = ref(DataBase, `childrenProfiles/${user.uid}/points`);
+                const pointUpdate = onValue(pointHent, (snapshot) => {
+                    const point = snapshot.val() || 0;
+                    setPoint(point);
+                });
+                return () => off(pointHent, "value", pointUpdate);
+            } else {
+                setPoint(0);
+            }
+        });      
+        return () => checkLogin();
+    }, []);
 
     return (
         <div className="point-tæller">
             <p>{point} ★</p>
         </div>
     );
-
-}
+} 
