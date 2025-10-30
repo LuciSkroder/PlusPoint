7beb7a206c86b66388b3a98dd53bf5df03bbb73d
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-10-30 11:53:50 +0100

    Refactor CSS styles for task page buttons

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./css/homepage.css";
 import "./css/shop.css";
 import "./css/task.css";
 import "./css/login.css";
+import "./css/taskpage.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
