651613c69077c620f73e2adfaefd137609394381
Luci Gabriella Skrøder <Luci.skroder@gmail.com>
2025-11-02 20:01:05 +0100

    navbar fix test

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ import "../css/navbar.css";
 function GåTilbage() {
   const navigate = useNavigate();
   return (
-    <button className="tilbage-knap" onClick={() => navigate("/")}>
+    <button className="tilbage-knap" onClick={() => navigate("/PlusPoint/")}>
       ➜
     </button>
   );
